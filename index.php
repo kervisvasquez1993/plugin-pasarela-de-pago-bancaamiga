24c11dc11bbb53a0fd6dabedeb0d65b06f4112ab
kervis1993 <kervisvasquez24@gmail.com>
2023-04-22 22:10:12 -0400

    update test solo falta el frontend en finalizar compra

--- index.php
+++ index.php
@@ -12,184 +12,127 @@ License: GPL2
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
-add_action('plugins_loaded', 'mi_plugin_cargar', 1);
 
-function mi_plugin_cargar()
+// Agregar el método de pago a la sección de ajustes de pagos de WooCommerce
+add_filter('woocommerce_payment_gateways', 'agregar_metodo_pago_personalizado');
+function agregar_metodo_pago_personalizado($gateways)
 {
-    // Cargar tu plugin aquí
-
-    class Mi_Plugin_Gateway extends WC_Payment_Gateway
-    {
-
-        public function __construct()
-        {
-            $this->id = 'mi_plugin_gateway';
-            $this->method_title = __('Banca Amiga', 'mi-plugin');
-            $this->method_description = __('Pago seguro con tarjeta Internacional', 'mi-plugin');
-            $this->supports = array('products');
-        }
-
-        public function init_form_fields()
-        {
-            $this->form_fields = array(
-                'enabled' => array(
-                    'title' => __('Habilitar/Deshabilitar', 'mi-plugin'),
-                    'type' => 'checkbox',
-                    'label' => __('Habilitar Mi Plugin Gateway', 'mi-plugin'),
-                    'default' => 'no',
-                ),
-            );
-        }
-
-        public function process_payment($order_id)
-        {
-            // Procesar el pago aquí
-        }
-
-        public function get_title()
-        {
-            return $this->method_title;
-        }
-
-        public function get_description()
-        {
-            return $this->method_description;
-        }
-
-        public function get_icon()
-        {
-            // Retornar el icono de la pasarela de pago aquí
-        }
-
-        public function is_available()
-        {
-            // Retornar si la pasarela de pago está disponible para su uso aquí
-        }
-
-        public function payment_fields()
-        {
-            // Mostrar los campos de pago aquí
-        }
-
-        public function receipt_page($order_id)
-        {
-            // Mostrar el formulario de pago aquí
-        }
-
-        public function thankyou_page()
-        {
-            // Mostrar información adicional después de que se haya completado el pago
-        }
-
-    }
-
-    function mi_plugin_registrar_gateway($gateways)
-    {
-        $gateways[] = 'Mi_Plugin_Gateway';
-        return $gateways;
-    }
-    add_filter('woocommerce_payment_gateways', 'mi_plugin_registrar_gateway');
-    add_action('woocommerce_checkout_before_order_review', 'mi_plugin_formulario_de_pago');
-    function mi_plugin_formulario_de_pago()
-    {
-        // Obtiene el monto total del carrito de Woocommerce
-        $monto = WC()->cart->total;
-        ?>
-    <div id="mi-plugin-pago">
-        <h3><?php _e('Pago seguro con tarjeta', 'mi-plugin');?></h3>
-        <form method="post" id="mi-plugin-pago-formulario">
-            <div class="form-row form-row-wide">
-                <label for="mi-plugin-pago-monto"><?php _e('Monto', 'mi-plugin');?> <span class="required">*</span></label>
-                <input type="text" class="input-text" name="mi-plugin-pago-monto" id="mi-plugin-pago-monto" value="<?php echo esc_attr($monto); ?>" readonly>
-            </div>
-            <div class="form-row form-row-wide">
-                <label for="mi-plugin-pago-numero"><?php _e('Número de tarjeta', 'mi-plugin');?> <span class="required">*</span></label>
-                <input type="text" class="input-text" name="mi-plugin-pago-numero" id="mi-plugin-pago-numero" placeholder="<?php esc_attr_e('Número de tarjeta', 'mi-plugin');?>" required>
-            </div>
-            <div class="form-row form-row-wide">
-                <label for="mi-plugin-pago-mes"><?php _e('Mes de expiración', 'mi-plugin');?> <span class="required">*</span></label>
-                <input type="text" class="input-text" name="mi-plugin-pago-mes" id="mi-plugin-pago-mes" placeholder="<?php esc_attr_e('MM', 'mi-plugin');?>" required>
-            </div>
-            <div class="form-row form-row-wide">
-                <label for="mi-plugin-pago-ano"><?php _e('Año de expiración', 'mi-plugin');?> <span class="required">*</span></label>
-                <input type="text" class="input-text" name="mi-plugin-pago-ano" id="mi-plugin-pago-ano" placeholder="<?php esc_attr_e('YYYY', 'mi-plugin');?>" required>
-            </div>
-            <div class="form-row form-row-wide">
-                <label for="mi-plugin-pago-cvc"><?php _e('CVC', 'mi-plugin');?> <span class="required">*</span></label>
-                <input type="text" class="input-text" name="mi-plugin-pago-cvc" id="mi-plugin-pago-cvc" placeholder="<?php esc_attr_e('CVC', 'mi-plugin');?>" required>
-            </div>
-            <div class="form-row form-row-wide">
-                <label for="mi-plugin-pago-direccion"><?php _e('Dirección de facturación', 'mi-plugin');?> <span class="required">*</span></label>
-                <input type="text" class="input-text" name="mi-plugin-pago-direccion" id="mi-plugin-pago-direccion" placeholder="<?php esc_attr_e('Dirección de facturación', 'mi-plugin');?>" required>
-            </div>
-            <div class="form-row form-row-wide">
-                <label for="mi-plugin-pago-referencia"><?php _e('Referencia', 'mi-plugin');?> <span class="required">*</span></label>
-                <input type="text" class="input-text" name="mi-plugin-pago-referencia" id="mi-plugin-pago-referencia" placeholder="<?php esc_attr_e('Referencia', 'mi-plugin');?>" required>
-            </div>
-            <?php wp_nonce_field('mi-plugin-pago', 'mi-plugin-pago-nonce');?>
-            <button type="submit" class="button alt" id="mi_funcion_procesar_pago"><?php _e('Pagar', 'mi-plugin');?></button>
-        </form>
-    </div>
-    <?php
+    $gateways[] = 'WC_Metodo_Pago_Personalizado';
+    return $gateways;
 }
 
-    add_action('woocommerce_checkout_process', 'mi_funcion_procesar_pago');
-    // add_action('init', 'mi_funcion_procesar_pago');
-    function mi_funcion_procesar_pago()
+// Declarar la clase del método de pago personalizado
+add_action('plugins_loaded', 'inicializar_metodo_pago_personalizado');
+function inicializar_metodo_pago_personalizado()
+{
+    class WC_Metodo_Pago_Personalizado extends WC_Payment_Gateway
     {
-        echo "hola";
-        if (isset($_POST['mi-plugin-pago-nonce']) && wp_verify_nonce($_POST['mi-plugin-pago-nonce'], 'mi-plugin-pago')) {
-            // Obtiene los datos del formulario
-            $monto = $_POST['mi-plugin-pago-monto'];
-            $numero = $_POST['mi-plugin-pago-numero'];
-            $mes = $_POST['mi-plugin-pago-mes'];
-            $ano = $_POST['mi-plugin-pago-ano'];
-            $direccion = $_POST['mi-plugin-pago-direccion'];
-            $cvc = $_POST['mi-plugin-pago-cvc'];
-            $referencia = $_POST['mi-plugin-pago-referencia'];
-            // Consume el servicio adicional para procesar el pago
-            $url = 'http://devpagos.sitca-ve.com/api/v1/cargo';
-            $args = array(
-                'method' => 'POST',
-                'headers' => array(
-                    'apikey' => 'pk4458cf90-2512-300b-b021-722c526f03c3',
+        public function __construct()
+        {
+            // Información del método de pago que aparecerá en la página de ajustes de pagos de WooCommerce
+            $this->id = 'metodo_pago_personalizado';
+            $this->method_title = 'Método de pago personalizado';
+            $this->method_description = 'Este método de pago permite cobrar con tarjeta de pago internacional a través de un servicio externo.';
+            $this->has_fields = true;
+
+            // Campos del formulario de pago que se mostrarán en la página de finalizar compra
+            $this->form_fields = array(
+                'numero_tarjeta' => array(
+                    'type' => 'text',
+                    'label' => 'Número de tarjeta',
+                    'placeholder' => 'Numero de tarjeta',
+                    'required' => true,
                 ),
-                'body' => array(
-                    'monto' => $monto,
-                    'numero' => $numero,
-                    'mes' => $mes,
-                    'ano' => $ano,
-                    'direccion' => $direccion,
-                    'cvc' => $cvc,
-                    'referencia' => $referencia,
+                'nombre' => array(
+                    'type' => 'text',
+                    'label' => 'Nombre',
+                    'required' => true,
+                ),
+                'mes' => array(
+                    'type' => 'text',
+                    'label' => 'Mes',
+                    'required' => true,
+                ),
+                'ano' => array(
+                    'type' => 'text',
+                    'label' => 'Año',
+                    'required' => true,
+                ),
+                'cvc' => array(
+                    'type' => 'text',
+                    'label' => 'CVC',
+                    'required' => true,
                 ),
             );
-            $response = wp_safe_remote_post($url, $args);
-            echo "respuesta:";
-            if (is_wp_error($response)) {
-                // Error al responder la solicitud
-                echo "error: ";
-                wc_add_notice('No se pudo procesar el pago. Inténtalo de nuevo más tarde.', 'error');
-            } else {
-                // Solicitud enviada correctamente
-                $body = wp_remote_retrieve_body($response);
-                $respuesta = json_decode($body);
-                echo $respuesta;
-                if ($respuesta->ok) {
-                    $order = wc_create_order();
-                    // crea aqui las opciones para procesar el pagoen woocommerce
-                    // Procesar el pago correctamente
-                    // $payment_gateway = wc_get_payment_gateway_by_order($order);
-                    // $payment_gateway->process_payment($order_id);
-                    echo "registrado correctamente";
-                    // Agrega aquí cualquier otro método que necesites ejecutar después de procesar el pago
-                } else {
-                    // Error al procesar el pago
-                    wc_add_notice('No se pudo procesar el pago. Inténtalo de nuevo más tarde.', 'error');
-                }
+
+            // Acción que se ejecutará cuando se envíe el formulario de pago
+            add_action('woocommerce_checkout_process', array($this, 'procesar_pago'));
+
+            // Acción que se ejecutará cuando se complete la orden de compra
+            add_action('woocommerce_order_status_completed', array($this, 'completar_orden'));
+        }
+
+        // Función que procesa el pago
+        public function procesar_pago()
+        {
+            // Obtener los datos del formulario de pago
+            $numero_tarjeta = isset($_POST['numero_tarjeta']) ? sanitize_text_field($_POST['numero_tarjeta']) : '';
+            $nombre = isset($_POST['nombre']) ? sanitize_text_field($_POST['nombre']) : '';
+            $mes = isset($_POST['mes']) ? sanitize_text_field($_POST['mes']) : '';
+            $ano = isset($_POST['ano']) ? sanitize_text_field($_POST['ano']) : '';
+            $cvc = isset($_POST['cvc']) ? sanitize_text_field($_POST['cvc']) : '';
+
+            // Validar los datos del formulario de pago
+            if (empty($numero_tarjeta) || empty($nombre) || empty($mes) || empty($ano) || empty($cvc)) {
+                wc_add_notice('Por favor, complete todos los campos del formulario de pago.', 'error');
+                return;
             }
+
+            // Enviar los datos de pago al servicio externo
+            $url = 'https://devpagos.sitca-ve.com/api/v1/cargo';
+            $apikey = 'pk4458cf90-2512-300b-b021-722c526f03c3';
+            $headers = array(
+                'apikey' => $apikey,
+                'Content-Type' => 'application/x-www-form-urlencoded',
+            );
+            $body = array(
+                'numero_tarjeta' => $numero_tarjeta,
+                'nombre' => $nombre,
+                'mes' => $mes,
+                'ano' => $ano,
+                'cvc' => $cvc,
+            );
+            $args = array(
+                'method' => 'POST',
+                'headers' => $headers,
+                'body' => http_build_query($body),
+            );
+            $response = wp_remote_post($url, $args);
+
+            // Validar la respuesta del servicio externo
+            if (is_wp_error($response)) {
+                wc_add_notice('Hubo un error al procesar el pago. Por favor, inténtelo de nuevo más tarde.', 'error');
+                return;
+            }
+            $response_code = wp_remote_retrieve_response_code($response);
+            $response_body = wp_remote_retrieve_body($response);
+            $response_data = json_decode($response_body);
+            if ($response_code != 200 || empty($response_data) || !isset($response_data->ok) || !$response_data->ok) {
+                wc_add_notice('Hubo un error al procesar el pago. Por favor, inténtelo de nuevo más tarde.', 'error');
+                return;
+            }
+
+            // Marcar la orden de compra como completada y mostrar un mensaje al usuario
+            $orden_id = wc_get_order_id_by_order_key($_POST['woocommerce-order-key']);
+            $orden = wc_get_order($orden_id);
+            $orden->update_status('completed', 'Pago procesado con éxito mediante el método de pago personalizado.');
+            wc_add_notice('¡Gracias por su compra! Su pago ha sido procesado con éxito.', 'success');
+        }
+
+        // Función que se ejecuta cuando se completa la orden de compra
+        public function completar_orden($orden_id)
+        {
+            // Agregar aquí cualquier acción adicional que necesite realizar cuando se complete la orden de compra
         }
     }
-
-    // add_action('woocommerce_thankyou', 'mi_plugin_procesa_pago');
 }
